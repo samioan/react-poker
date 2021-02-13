c4ca1733a1e0aac71a259ac98ba8b2872fbe682a
Ioannis Siampalias <ioannis.siampalias@gmail.com>
2021-02-13 17:41:17 +0200

    game logic moved to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { Board, Intro } from "./components/game/components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 
-import Game from "./components/game/";
-
-const App = () => <Game />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Intro />
+      </Route>
+    </Switch>
+    <Switch>
+      <Route path="/game">
+        <Board />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export { App };
 export default App;
