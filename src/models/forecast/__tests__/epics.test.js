0d9b51c095017ae81c4e22ac02bb543c9743b5db
Ioannis Siampalias <ioannis.siampalias@gmail.com>
2021-02-18 15:31:59 +0200

    setting up epic tests so i can work on them later

---
+++ src/models/forecast/__tests__/epics.test.js
@@ -0,0 +1,55 @@
+// import { getForecastEpic, getForecastSucceededEpic } from "../epics";
+// import { initialState } from "../reducer";
+// import { getForecast, forecastLoaded } from "../actions";
+// import * as services from "../services";
+
+// describe("Game epics", () => {
+//   describe("startGameEpic", () => {
+//     test("should emit stopGame action, when setTicTacToe is emitted and state has a winner", () => {
+//       // GIVEN
+//       const inputAction = setTicTacToe();
+
+//       // WHEN
+//       const { epicEmissions, emitAction } = testEpic(checkGameFinishedEpic, {
+//         ...initialState,
+//         ticTacToe: {
+//           winner: "WinnerName",
+//         },
+//       });
+//       emitAction(inputAction);
+
+//       // THEN
+//       expect(epicEmissions.length).toBe(1);
+//       expect(epicEmissions[0]).toEqual(stopGame());
+//     });
+
+//     describe("joinGameEpic", () => {
+//       test("effect operator should have been called joinGameRequest, with state default name as a parameter", () => {
+//         // GIVEN
+//         const inputAction = joinGame();
+//         const joinGameRequestSpy = jest
+//           .spyOn(services, "joinGameRequest")
+//           .mockReturnValue({});
+
+//         // WHEN
+//         const { epicEmissions, emitAction } = testEpic(joinGameEpic, {
+//           ...initialState,
+//           players: {
+//             defaultName: "ADefaultName",
+//           },
+//         });
+
+//         emitAction(inputAction);
+//         epicEmissions[0].payload.effectCreator();
+
+//         // THEN
+//         expect(joinGameRequestSpy).toHaveBeenCalledWith("ADefaultName");
+//         expect(epicEmissions[0].payload.effectResponseAction).toEqual(joinGame);
+//         expect(epicEmissions[0].payload.additionalResponseActions).toEqual([
+//           startGame,
+//         ]);
+//         joinGameRequestSpy.mockRestore();
+//       });
+//     });
+//   });
+// });
